9aef71d6802af45c9b050fd3808e13f9846b736a
Yovo Manolov <yovo.manolov@abv.bg>
2024-11-18 01:20:01 +0200

    Operators tests.

---
+++ src/test/java/academy/dev/dojo/reactive/test/OperatorsTest.java
@@ -0,0 +1,154 @@
+package academy.dev.dojo.reactive.test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
+
+@Slf4j
+public class OperatorsTest {
+
+  @Test
+  public void subscribeOnSimple() {
+    Flux<Integer> flux =
+        Flux.range(1, 4)
+            .map(
+                i -> {
+                  log.info("Map 1 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                  return i;
+                })
+            // when we apply subscribeOn it affects everything that the publisher is going to
+            // publish
+            .subscribeOn(Schedulers.single())
+            .map(
+                i -> {
+                  log.info("Map 2 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                  return i;
+                });
+
+    StepVerifier.create(flux).expectSubscription().expectNext(1, 2, 3, 4).verifyComplete();
+  }
+
+  @Test
+  public void publishOnSimple() {
+    Flux<Integer> flux =
+        Flux.range(1, 4)
+            .map(
+                i -> {
+                  log.info("Map 1 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                  return i;
+                })
+            .publishOn(Schedulers.boundedElastic())
+            .map(
+                i -> {
+                  log.info("Map 2 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                  return i;
+                });
+    StepVerifier.create(flux).expectSubscription().expectNext(1, 2, 3, 4).verifyComplete();
+  }
+
+  @Test
+  public void multipleSubscribeOnSimple() {
+    Flux<Integer> flux =
+        Flux.range(1, 4)
+            .subscribeOn(Schedulers.boundedElastic())
+            .map(
+                i -> {
+                  log.info("Map 1 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                  return i;
+                })
+            .subscribeOn(Schedulers.single())
+            .map(
+                i -> {
+                  log.info("Map 2 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                  return i;
+                });
+    StepVerifier.create(flux).expectSubscription().expectNext(1, 2, 3, 4).verifyComplete();
+  }
+
+  @Test
+  public void multiplePublishOnSimple() {
+    Flux<Integer> flux =
+        Flux.range(1, 4)
+            .publishOn(Schedulers.single())
+            .map(
+                i -> {
+                  log.info("Map 1 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                  return i;
+                })
+            // (publishOn) Typical Use Case: To switch to a different thread at a specific point in
+            // the pipeline.
+            .publishOn(Schedulers.boundedElastic())
+            .map(
+                i -> {
+                  log.info("Map 2 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                  return i;
+                });
+    StepVerifier.create(flux).expectSubscription().expectNext(1, 2, 3, 4).verifyComplete();
+  }
+
+  @Test
+  public void publishAndSubscribeOnSimple() {
+    Flux<Integer> flux =
+        Flux.range(1, 4)
+            .publishOn(Schedulers.single())
+            .map(
+                i -> {
+                  log.info("Map 1 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                  return i;
+                })
+            .subscribeOn(Schedulers.boundedElastic())
+            .map(
+                i -> {
+                  log.info("Map 2 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                  return i;
+                });
+    StepVerifier.create(flux).expectSubscription().expectNext(1, 2, 3, 4).verifyComplete();
+  }
+
+  @Test
+  public void subscribeAndPublishOnSimple() {
+    Flux<Integer> flux =
+        Flux.range(1, 4)
+            .subscribeOn(Schedulers.single())
+            .map(
+                i -> {
+                  log.info("Map 1 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                  return i;
+                })
+            .publishOn(Schedulers.boundedElastic())
+            .map(
+                i -> {
+                  log.info("Map 2 - Number {} on Thread {}", i, Thread.currentThread().getName());
+                  return i;
+                });
+    StepVerifier.create(flux).expectSubscription().expectNext(1, 2, 3, 4).verifyComplete();
+  }
+
+  @Test
+  public void subscribeOnIO() throws InterruptedException {
+    Mono<List<String>> list =
+        Mono.fromCallable(() -> Files.readAllLines(Path.of("text-file")))
+            .log()
+            .subscribeOn(Schedulers.boundedElastic());
+
+    list.subscribe(s -> log.info("{}", s));
+    Thread.sleep(2000);
+
+    StepVerifier.create(list)
+        .expectSubscription()
+        .thenConsumeWhile(
+            l -> {
+              Assertions.assertFalse(l.isEmpty());
+              log.info("Size {} ", l.size());
+              return true;
+            })
+        .verifyComplete();
+  }
+}
